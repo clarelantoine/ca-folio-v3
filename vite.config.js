1babcea88090e208cfa6ef5ae305496a161b482b
Clarel Antoine <clarel.antoine93@gmail.com>
2023-10-16 23:30:14 +0400

    first commit

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import glsl from 'vite-plugin-glsl';
+
+const isCodeSandbox =
+    'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env;
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react(), glsl()],
+
+    // root: 'src/',
+    publicDir: './public',
+    base: './',
+    server: {
+        host: true,
+        open: !isCodeSandbox, // Open if it's not a CodeSandbox
+    },
+    build: {
+        outDir: '../dist',
+        emptyOutDir: true,
+        sourcemap: true,
+    },
+});
