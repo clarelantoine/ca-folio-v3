3590648e831a09633fab050b798136d40043ad78
Clarel Antoine <clarel.antoine93@gmail.com>
2023-10-18 03:15:22 +0400

    added webgl image slider

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,23 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
-import { GlobalCanvas, SmoothScrollbar } from '@14islands/r3f-scroll-rig';
 import { BrowserRouter } from 'react-router-dom';
 
 import './index.scss';
+import { PerspectiveCamera, Preload } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
 import App from './App';
+import Slider from './components/slider/slider.component';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <BrowserRouter>
-            <GlobalCanvas>
-                {(globalChildren) => <Suspense>{globalChildren}</Suspense>}
-            </GlobalCanvas>
-            <SmoothScrollbar />
+            <Canvas gl={{ antialias: false }} dpr={[1, 1.5]}>
+                <ambientLight intensity={1.5} />
+                <Suspense>
+                    <Slider />
+                    <Preload />
+                </Suspense>
+            </Canvas>
             <App />
         </BrowserRouter>
     </React.StrictMode>
--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,10 +1,8 @@
-import Slider from '../../components/slider/slider.component';
 import './home.styles.scss';
 
 const Home = () => (
     <div className="home page">
         <h1>Home page</h1>
-        <Slider />
     </div>
 );
 
