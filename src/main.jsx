1fda7135c26ad6bfced7cb54b2c355864e543376
Clarel Antoine <clarel.antoine93@gmail.com>
2023-10-22 23:54:50 +0400

    refactored project

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,25 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Canvas } from '@react-three/fiber';
+
+import App from './App';
+import Experience from './components/Experience';
 
 import './index.scss';
-import { PerspectiveCamera, Preload } from '@react-three/drei';
-import { Canvas } from '@react-three/fiber';
-import App from './App';
-import Slider from './components/slider/slider.component';
+import { PortfolioProvider } from './contexts/portfolio.context';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <BrowserRouter>
-            <Canvas gl={{ antialias: false }} dpr={[1, 1.5]}>
-                <ambientLight intensity={1.5} />
-                <Suspense>
-                    <Slider />
-                    <Preload />
-                </Suspense>
-            </Canvas>
-            <App />
+            <PortfolioProvider>
+                <Canvas gl={{ antialias: false }} dpr={[1, 1.5]}>
+                    <Suspense>
+                        <Experience />
+                    </Suspense>
+                </Canvas>
+                <App />
+            </PortfolioProvider>
         </BrowserRouter>
     </React.StrictMode>
 );
